883d8d4941e3ccee2e5b807a79c29bac17d98311
Iamtheusername112 <apkfem@gmail.com>
2023-07-27 13:35:32 +0200

    working on the App.jsx and protecting the routes

--- Guideme-client/src/App.jsx
+++ Guideme-client/src/App.jsx
@@ -1,26 +1,43 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/home/Home";
 import Login from "./components/login/Login";
 import Signup from "/src/components/signup/Signup";
 import Upload from "./components/upload/Upload";
 import ProfileDetail from "./components/profileDetail/ProfileDetail";
 import PostDetails from "./components/postDetails/PostDetails";
+import { useSelector } from "react-redux";
 // import { Link } from "react-router-dom";
 
 function App() {
+  const { user } = useSelector((state) => state.auth);
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/profileDetail/:id" element={<ProfileDetail />} />
-        <Route path="/postDetials/:id" element={<PostDetails />} />
+        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={!user ? <Signup /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/upload"
+          element={user ? <Upload /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/profileDetail/:id"
+          element={user ? <ProfileDetail /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/postDetails/:id"
+          element={user ? <PostDetails /> : <Navigate to="/login" />}
+        />
       </Routes>
       <Footer />
     </div>
